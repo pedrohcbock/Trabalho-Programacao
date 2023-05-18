ae74bb46963639720327dc894898a9c27c759608
ThePaleLady <anaguisilveira61@gmail.com>
2023-05-17 22:21:31 -0300

    se o usuario tiver documentos associados a ele ele n pode ser removido

--- usuario_apagar.php
+++ usuario_apagar.php
@@ -1,28 +1,35 @@
 <?php
-    # usuario_apagar.php
-    require('carregar_twig.php');
-    require('pdo.php'); // Conexão com o banco
+require('carregar_twig.php');
+require('pdo.php'); // Conexão com o banco
 
-    // Rotina de POST - Apagar o usuário
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Modifica o usuário para ativo = 0
-        $id = $_POST['id'] ?? false;
-        if ($id) {
+// Rotina de POST - Apagar o usuário
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Modifica o usuário para ativo = 0
+    $id = $_POST['id'] ?? false;
+    if ($id) {
+        try {
             $sql = $pdo->prepare('DELETE FROM usuarios WHERE id = ?');
             $sql->execute([$id]);
+        } catch (PDOException $e) {
+            if ($e->getCode() == '23000' && strpos($e->getMessage(), 'compartilhamentos') !== false) {
+                echo 'O usuário não pode ser apagado, pois existem documentos importantes associados a ele. <a href="usuarios.php">Voltar</a>';
+                die;
+            } else {
+                echo 'Ocorreu um erro ao apagar o usuário: ' . $e->getMessage();
+                die;
+            }
         }
         header('location:usuarios.php');
         die;
     }
+}
 
-    // Rotina de GET - Mostrar informações na tela
+$id = $_GET['id'] ?? false;
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE id = ?');
+$sql->execute([$id]);
+$usuario = $sql->fetch(PDO::FETCH_ASSOC);
 
-    // Busca o usuário no banco para mostrar o nome dele na tela
-    $id = $_GET['id'] ?? false;
-    $sql = $pdo->prepare('SELECT * FROM usuarios WHERE id = ?');
-    $sql->execute([$id]);
-    $usuario = $sql->fetch(PDO::FETCH_ASSOC);
-
-    echo $twig->render('/usuario_apagar.html',[
-        'usuario' => $usuario,
-    ]);
+echo $twig->render('/usuario_apagar.html', [
+    'usuario' => $usuario,
+]);
+?>
